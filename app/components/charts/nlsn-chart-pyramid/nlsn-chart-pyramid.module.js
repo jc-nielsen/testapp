4664215908594caa0529a3309b58e31d2f72899f
JC Lango <jc.lango.consultant@nielsen.com>
2017-05-31 11:29:41 -0400

    json data renders, needs scale left and right

--- app/components/charts/nlsn-chart-pyramid/nlsn-chart-pyramid.module.js
+++ app/components/charts/nlsn-chart-pyramid/nlsn-chart-pyramid.module.js
@@ -1,119 +1,115 @@
 'use strict';
 
 angular.module('nlsnChart.Pyramid.module', [])
-
   .directive('nlsnChartPyramid', [
     'nlsnDataSvc',
     function (nlsnDataSvc) {
-
-    var mySvg;
-
+      var mySvg;
+      var data;
       var theController = function ($scope) {
         $scope.chartData = nlsnDataSvc.getChartDataPyramid();
       };
 
-      function renderChart(){
-        console.log('renderChart');
+      return {
+        bindToController: true,
+        controller: theController,
+        //controllerAs: 'vm',
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'components/charts/nlsn-chart-pyramid/nlsn-chart-pyramid.html',
+        link: function (scope, element) {
+          mySvg = d3.select(element[0]).append("svg");
+          scope.$watch("chartData", renderChart, true);
+        }
+      };
+
+      function renderChart(newValue, oldValue, scope) {
+        if (!(newValue && newValue.data && newValue.data.length)) {
+          return;
+        }
+
+        var chartData = newValue;
 
         mySvg.attr("width", 600)
           .attr("height", 400);
 
-        /* edit/input your data */
-        var data = [
-          {"sharedLabel": "Category 1", "barData1": 43041, "barData2": 40852},
-          {"sharedLabel": "Category 2", "barData1": 38867, "barData2": 36296},
-          {"sharedLabel": "Category 3", "barData1": 41748, "barData2": 40757},
-          {"sharedLabel": "Category 4", "barData1": 24831, "barData2": 23624},
-          {"sharedLabel": "Category 5", "barData1": 15764, "barData2": 15299},
-          {"sharedLabel": "Category 6", "barData1": 17006, "barData2": 16071},
-          {"sharedLabel": "Category 7", "barData1": 24309, "barData2": 23235},
-          {"sharedLabel": "Category 8", "barData1": 46756, "barData2": 46065},
-          {"sharedLabel": "Category 9", "barData1": 41923, "barData2": 41704},
-          {"sharedLabel": "Category 10", "barData1": 42565, "barData2": 42159},
-          {"sharedLabel": "Category 11", "barData1": 44316, "barData2": 45468},
-          {"sharedLabel": "Category 12", "barData1": 42975, "barData2": 44223},
-          {"sharedLabel": "Category 13", "barData1": 36755, "barData2": 39452},
-          {"sharedLabel": "Category 14", "barData1": 31578, "barData2": 34063},
-          {"sharedLabel": "Category 15", "barData1": 10328, "barData2": 11799},
-          {"sharedLabel": "Category 16", "barData1": 13917, "barData2": 14949},
-          {"sharedLabel": "Category 17", "barData1": 7920, "barData2": 8589},
-          {"sharedLabel": "Category 18", "barData1": 9003, "barData2": 10397},
-          {"sharedLabel": "Category 19", "barData1": 14322, "barData2": 16832},
-          {"sharedLabel": "Category 20", "barData1": 12369, "barData2": 15836},
-          {"sharedLabel": "Category 21", "barData1": 8710, "barData2": 12377},
-          {"sharedLabel": "Category 22", "barData1": 5853, "barData2": 12213}
-        ];
+        data = chartData.data;
 
         /* edit these settings freely */
         var w = 600,
           h = 400,
           topMargin = 15,
           labelSpace = 40,
-          innerMargin = w/2+labelSpace,
+          innerMargin = w / 2 + labelSpace,
           outerMargin = 15,
-          gap = 2,
-          dataRange = d3.max(data.map(function(d) { return Math.max(d.barData1, d.barData2) }));
+          gap = 2;
 
-        var leftLabel = "Left label",
-          rightLabel = "Right label";
+        var dataRange = d3.max(data.map(function (d) {
+          return Math.max(d.metric1, d.metric2)
+        }));
+
+        var dataRange1 = d3.max(data, function (d) {
+          return d.metric1;
+        });
+        var dataRange2 = d3.max(data, function (d) {
+          return d.metric2;
+        });
+
+        var leftLabel = chartData.metric1Label,
+          rightLabel = chartData.metric2Label;
 
         /* edit with care */
         var chartWidth = w - innerMargin - outerMargin,
           barWidth = h / data.length,
-          yScale = d3.scale.linear().domain([0, data.length]).range([0, h-topMargin]),
+          yScale = d3.scale.linear().domain([0, data.length]).range([0, h - topMargin]),
           total = d3.scale.linear().domain([0, dataRange]).range([0, chartWidth - labelSpace]),
           commas = d3.format(",.0f");
 
-        /* main panel */
-        // var vis = d3.select("#vis").append("svg")
-        //   .attr("width", w)
-        //   .attr("height", h);
-
-        /* barData1 label */
+        /* metric1 label */
         mySvg.append("text")
           .attr("class", "label")
           .text(leftLabel)
-          .attr("x", w-innerMargin)
-          .attr("y", topMargin-3)
+          .attr("x", w - innerMargin)
+          .attr("y", topMargin - 3)
           .attr("text-anchor", "end");
 
-        /* barData2 label */
+        /* metric2 label */
         mySvg.append("text")
           .attr("class", "label")
           .text(rightLabel)
           .attr("x", innerMargin)
-          .attr("y", topMargin-3);
+          .attr("y", topMargin - 3);
 
         /* female bars and data labels */
         var bar = mySvg.selectAll("g.bar")
           .data(data)
           .enter().append("g")
           .attr("class", "bar")
-          .attr("transform", function(d, i) {
+          .attr("transform", function (d, i) {
             return "translate(0," + (yScale(i) + topMargin) + ")";
           });
 
         var wholebar = bar.append("rect")
           .attr("width", w)
-          .attr("height", barWidth-gap)
-          .attr("fill", "none")
-          .attr("pointer-events", "all");
+          .attr("height", barWidth - gap)
+          .attr("fill", "none");
+//          .attr("pointer-events", "all");
 
-        var highlight = function(c) {
-          return function(d, i) {
-            bar.filter(function(d, j) {
-              return i === j;
-            }).attr("class", c);
-          };
-        };
+        // var highlight = function (c) {
+        //   return function (d, i) {
+        //     bar.filter(function (d, j) {
+        //       return i === j;
+        //     }).attr("class", c);
+        //   };
+        // };
 
-        bar
-          .on("mouseover", highlight("highlight bar"))
-          .on("mouseout", highlight("bar"));
+        // bar
+        //   .on("mouseover", highlight("highlight bar"))
+        //   .on("mouseout", highlight("bar"));
 
         bar.append("rect")
           .attr("class", "femalebar")
-          .attr("height", barWidth-gap);
+          .attr("height", barWidth - gap);
 
         bar.append("text")
           .attr("class", "femalebar")
@@ -123,7 +119,7 @@ angular.module('nlsnChart.Pyramid.module', [])
 
         bar.append("rect")
           .attr("class", "malebar")
-          .attr("height", barWidth-gap)
+          .attr("height", barWidth - gap)
           .attr("x", innerMargin);
 
         bar.append("text")
@@ -134,18 +130,20 @@ angular.module('nlsnChart.Pyramid.module', [])
         /* sharedLabels */
         bar.append("text")
           .attr("class", "shared")
-          .attr("x", w/2)
+          .attr("x", w / 2)
           .attr("dy", "1em")
           .attr("text-anchor", "middle")
-          .text(function(d) { return d.sharedLabel; });
+          .text(function (d) {
+            return d.sharedLabel;
+          });
 
-        d3.select("#generate").on("click", function() {
-          for (var i=0; i<data.length; i++) {
-            data[i].barData1 = Math.random() * dataRange;
-            data[i].barData2 = Math.random() * dataRange;
-          }
-          refresh(data);
-        });
+        // d3.select("#generate").on("click", function () {
+        //   for (var i = 0; i < data.length; i++) {
+        //     data[i].metric1 = Math.random() * dataRange;
+        //     data[i].metric2 = Math.random() * dataRange;
+        //   }
+        //   refresh(data);
+        // });
 
         refresh(data);
 
@@ -154,36 +152,36 @@ angular.module('nlsnChart.Pyramid.module', [])
             .data(data);
           bars.selectAll("rect.malebar")
             .transition()
-            .attr("width", function(d) { return total(d.barData1); });
+            .attr("width", function (d) {
+              return total(d.metric1);
+            });
           bars.selectAll("rect.femalebar")
             .transition()
-            .attr("x", function(d) { return innerMargin - total(d.barData2) - 2 * labelSpace; })
-            .attr("width", function(d) { return total(d.barData2); });
+            .attr("x", function (d) {
+              return innerMargin - total(d.metric2) - 2 * labelSpace;
+            })
+            .attr("width", function (d) {
+              return total(d.metric2);
+            });
 
           bars.selectAll("text.malebar")
-            .text(function(d) { return commas(d.barData1); })
+            .text(function (d) {
+              return commas(d.metric1);
+            })
             .transition()
-            .attr("x", function(d) { return innerMargin + total(d.barData1); });
+            .attr("x", function (d) {
+              return innerMargin + total(d.metric1);
+            });
           bars.selectAll("text.femalebar")
-            .text(function(d) { return commas(d.barData2); })
+            .text(function (d) {
+              return commas(d.metric2);
+            })
             .transition()
-            .attr("x", function(d) { return innerMargin - total(d.barData2) - 2 * labelSpace; });
+            .attr("x", function (d) {
+              return innerMargin - total(d.metric2) - 2 * labelSpace;
+            });
         }
       }
 
-      return {
-        bindToController: true,
-        controller: theController,
-        controllerAs: 'vm',
-        restrict: 'E',
-        scope: {},
-        templateUrl: 'components/charts/nlsn-chart-pyramid/nlsn-chart-pyramid.html',
-        link: function(scope, element) {
-
-          mySvg = d3.select(element[0]).append("svg");
-
-          scope.$watch("chartData", renderChart, true);
-        }
-      };
     }])
 ;
--- app/services/nlsn-data-svc.module.js
+++ app/services/nlsn-data-svc.module.js
@@ -3,11 +3,13 @@
 angular.module('nlsnDataSvc.module', [])
   .service('nlsnDataSvc', ['$resource', function ($resource) {
     this.getChartDataPyramid = function () {
+      // var pyramidChartApiUrl = '/public/data/chart/test.json';
       var pyramidChartApiUrl = '/public/data/chart/nlsn-chart-pyramid.sample.json';
       var theResource = $resource(pyramidChartApiUrl, null,
         {
           'query': {method: 'GET', isArray: false}
         });
+      // var theResource = $resource(pyramidChartApiUrl);
       return theResource.query();
     }
   }])
