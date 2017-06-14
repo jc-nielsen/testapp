546b5bfc6482970ae6732c5f1fe83943ccc5532b
jc-nielsen <jc.lango.consultant@nielsen.com>
2017-06-14 13:07:23 -0400

    added test folder for sandbox pages

--- app/components/charts/nlsn-chart-donut/nlsn-chart-donut.module.js
+++ app/components/charts/nlsn-chart-donut/nlsn-chart-donut.module.js
@@ -43,6 +43,7 @@ angular.module('nlsnChart.donut.module', [])
           getOptions(chart, chartData);
           getData(chart, chartData);
           getConfig(chart);
+          createPanels(chart);
           calculateSettings(chart);
           nlsnChartHelperSvc.drawBaseElement(chart);
           drawPie(chart);
@@ -56,7 +57,10 @@ angular.module('nlsnChart.donut.module', [])
         }
 
         function getData(chart, chartData) {
-          chart.data = chartData.data;
+          // Convert single metric to two metrics for pie chart data
+          chart.data = [100 - chartData.data[0].value, chartData.data[0].value];
+
+          chart.metrics = chartData.data;
         }
 
         // Configurable Settings - internal to component - not avail from any inputs.
@@ -64,12 +68,87 @@ angular.module('nlsnChart.donut.module', [])
           chart.config = {};
         }
 
+        // Panels are virtual objects that need to move or transform.
+        function createPanels(chart) {
+          // Create a panel for the center divider
+          chart.centerDividerPanel = {};
+
+          // The dataPanel is the box containing the data graph area, includes record labels.
+          chart.dataPanel = {};
+
+          // Create a panel for the pie chart.
+          chart.piePanel = {};
+
+          // Create a metrics panel for each metric.
+          chart.metricsPanel = [];
+          chart.metricsPanel[0] = {};
+          chart.metricsPanel[1] = {};
+          chart.metricsPanel[1] = {};
+
+          // Create an axis panel for each metric.
+          chart.metricsPanel[0].axisPanel = {};
+          chart.metricsPanel[1].axisPanel = {};
+
+          // Create a panel for the record labels
+          chart.recordLabelPanel = {};
+        }
+
         function calculateSettings(chart) {
-          chart.settings = {};
-          chart.settings.radius = Math.min(chart.options.width, chart.options.height) / 2;
+
+          // The dataPanel is the chart area bounded by margins.
+          chart.dataPanel.width = chart.options.width - (chart.options.margin.left + chart.options.margin.right);
+          chart.dataPanel.height = chart.options.height - (chart.options.margin.top + chart.options.margin.bottom);
+          chart.dataPanel.x = 0 + chart.options.margin.left;
+          chart.dataPanel.y = 0 + chart.options.margin.top;
+
+          chart.piePanel.radius = Math.min(chart.options.width, chart.options.height) / 2;
+          chart.piePanel.height = chart.piePanel.radius;
+          chart.piePanel.width = chart.piePanel.radius;
+          chart.piePanel.x = chart.dataPanel.x + chart.piePanel.width / 2;
+          chart.piePanel.y = chart.dataPanel.y + chart.piePanel.height / 2;
+
+          chart.piePanel.arc = d3.svg.arc()
+              .outerRadius(chart.piePanel.radius - 10)
+              .innerRadius(chart.piePanel.radius - 70);
         }
 
         function drawPie(chart) {
+          chart.piePanel.baseElement = chart.svgElement.append('g')
+              .attr('class', 'nlsn-chart-pie')
+              .attr('transform', 'translate(' + chart.piePanel.x + ',' + chart.piePanel.y + ')');
+          //.attr('x', chart.piePanel.x)
+          //.attr('y', chart.piePanel.y);
+
+          chart.piePanel.pie = d3.layout.pie()
+              .sort(null)
+              .value(function (d) {
+                return d.data;
+              });
+
+          chart.piePanel.arc = chart.piePanel.baseElement.append('arc')
+              .attr('class', 'nlsn-chart-arc')
+              .data(chart.piePanel.pie(chart.data));
+
+
+          chart.piePanel.color = d3.scale.ordinal()
+              .range([chart.options.metric0Color, chart.options.metric1Color]);
+
+          //chart.piePanel.arc.append('path')
+          //    //              .attr('d', chart.piePanel.arc)
+          //    .style('fill', function (d) {
+          //      return chart.piePanel.color(d.data);
+          //    });
+
+          var g = chart.piePanel.baseElement.selectAll('.arc')
+              .data(chart.piePanel.pie(chart.data))
+              .enter().append('g')
+              .attr('class', 'arc');
+
+          g.append('path')
+              .attr('d', chart.piePanel.arc)
+              .style('fill', function (d) {
+                return chart.piePanel.color(d.data);
+              });
 
         }
 
